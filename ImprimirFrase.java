8f35c2570f610c9ae3b3246feba0f9f6dfbafa75
GeorgeMendesMarra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2023-08-18 14:09:30 -0300

    Create ImprimirFrase.java

---
+++ ImprimirFrase.java
@@ -0,0 +1,9 @@
+public class ImprimirFrase {
+    public static void main(String[] args) {
+        // Defina a frase que você deseja imprimir
+        String frase = "Olá, mundo! Esta é uma frase qualquer.";
+
+        // Imprime a frase no console
+        System.out.println(frase);
+    }
+}
