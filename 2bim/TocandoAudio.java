479908e7f313e7254856eac89a7571da56a7b026
GeorgeMendesMarra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2023-11-23 21:05:18 -0300

    Update TocandoAudio.java

--- 2bim/TocandoAudio.java
+++ 2bim/TocandoAudio.java
@@ -5,25 +5,27 @@ import javafx.scene.media.AudioClip;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class TocandoAudio extends Application {
 
-	private String AUDIO_URL = getClass().getResource(
-			"/audio/Sean_Fournier_-_All_I_ll_Ever_Need.mp3").toString();
+    private String audioPath = new File(System.getProperty("user.home") + "/Downloads/Sean_Fournier_-_All_I_ll_Ever_Need.mp3").toURI().toString();
 
-	public static void main(String[] args) {
-		launch();
-	}
+    public static void main(String[] args) {
+        launch();
+    }
 
-	@Override
-	public void start(Stage palco) throws Exception {
-		AudioClip clip = new AudioClip(AUDIO_URL);// 1
-		clip.play(); // 2
-		StackPane raiz = new StackPane();
-		raiz.getChildren().add(new Text("Tocando M�sica ")); 
-		Scene cena = new Scene(raiz, 600, 100);
-		palco.setTitle("Tocando Audio em JavaFX");
-		palco.setScene(cena);
-		palco.show();
+    @Override
+    public void start(Stage palco) throws Exception {
+        AudioClip clip = new AudioClip(audioPath);
+        clip.play();
 
-	}
+        StackPane raiz = new StackPane();
+        raiz.getChildren().add(new Text("Tocando M�sica "));
+
+        Scene cena = new Scene(raiz, 600, 100);
+        palco.setTitle("Tocando Audio em JavaFX");
+        palco.setScene(cena);
+        palco.show();
+    }
 }
