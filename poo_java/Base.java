a2f4a770e5b0c28e78913bce7521e59c1f963a95
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-03-10 09:45:59 -0300

    Add files via upload

---
+++ poo_java/Base.java
@@ -0,0 +1,53 @@
+public class Base {
+    public static void main(String[] args) {
+        // Declaração e inicialização de uma variável booleana (verdadeiro/falso)
+        boolean flag = true;
+
+        // Declaração e inicialização de uma variável do tipo caractere
+        char ch = 'A';
+
+        // Declaração e inicialização de uma variável do tipo byte (inteiro de 8 bits)
+        byte b = 12;
+
+        // Declaração e inicialização de uma variável do tipo short (inteiro de 16 bits)
+        short s = 24;
+
+        // Declaração e inicialização de uma variável do tipo int (inteiro de 32 bits)
+        int i = 257;
+
+        // Declaração e inicialização de uma variável do tipo long (inteiro de 64 bits)
+        long l = 890L; // O 'L' no final indica que é um literal long
+
+        // Declaração e inicialização de uma variável do tipo float (ponto flutuante de 32 bits)
+        float f = 3.1415F; // O 'F' no final indica que é um literal float
+
+        // Declaração e inicialização de uma variável do tipo double (ponto flutuante de 64 bits)
+        double d = 2.1828;
+
+        // Imprime o valor da variável booleana flag no console
+        System.out.println("flag =" + flag);
+
+        // Imprime o valor da variável caractere ch no console
+        System.out.println("ch =" + ch);
+        
+        // Imprime o valor da variável byte b no console
+        System.out.println("b = " + b);
+
+        // Imprime o valor da variável short s no console
+        System.out.println("s = " + s);
+
+        // Imprime o valor da variável int i no console
+        System.out.println("i = " + i);
+
+        // Imprime o valor da variável long l no console
+        System.out.println("l = " + l);
+
+        // Imprime o valor da variável float f no console
+        System.out.println("f = " + f);
+
+        // Imprime o valor da variável double d no console
+        System.out.println("d = " + d);
+
+        // O programa termina aqui.
+    }
+}
