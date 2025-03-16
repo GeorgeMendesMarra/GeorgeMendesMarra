3d4cec8a22407c7221cc819e6060e06b00006c06
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-03-16 15:42:16 -0300

    Create hello_world5.py

---
+++ python/hello_world5.py
@@ -0,0 +1,7 @@
+nome = "mundo"
+print("Olá, {}!".format(nome))
+
+# print(): Esta é uma função embutida em Python que exibe a saída no console.
+# "Olá, {}!".format(nome): Esta é uma string formatada usando o método .format().
+# "Olá, {}!": Esta é a string principal, com um marcador {} que será substituído pelo valor da variável nome.
+#.format(nome): Este é o método .format() que substitui o marcador {} pelo valor da variável nome.
