ac48c3451a38713908fe716ea4cb04c883f95294
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-03-16 15:13:18 -0300

    Create hello_world4.py

---
+++ python/hello_world4.py
@@ -0,0 +1,5 @@
+# import: Esta é a palavra-chave que importa um módulo em Python.
+import sys
+#stdout: Este é um objeto dentro do módulo sys que representa a saída padrão (geralmente o console).
+sys.stdout.write("Hello, world!\n") # \n adiciona uma nova linha
+# write(): Este é um método do objeto stdout que escreve uma string na saída padrão.
