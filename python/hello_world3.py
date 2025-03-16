7f0011fda5f00f6e41c6386a68a8c03e7c392b2e
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-03-16 14:32:42 -0300

    Create hello_world3.py

---
+++ python/hello_world3.py
@@ -0,0 +1,12 @@
+def saudacao():
+    print("Hello, world!")
+
+# def: Esta é a palavra-chave que define uma função em Python.
+# saudacao: Este é o nome da função. Você pode escolher qualquer nome válido para sua função.
+# (): Estes parênteses indicam que a função não recebe nenhum argumento (entrada).
+# :: Os dois pontos indicam o início do bloco de código que pertence à função.
+
+saudacao()
+
+# saudacao: Este é o nome da função que estamos chamando.
+# (): Estes parênteses indicam que estamos chamando a função.
