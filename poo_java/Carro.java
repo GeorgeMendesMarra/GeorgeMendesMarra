22e66220b8adb6b904abe20dbfff357b0a12f687
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-03-16 21:43:21 -0300

    Add files via upload

---
+++ poo_java/Carro.java
@@ -0,0 +1,25 @@
+//Nome da classe: Carro - Sou um comentário de uma linha
+
+class Carro {     //Linha 3
+   String cor;    //Linha 4
+   String marca;  //Linha 5
+   String modelo; //Linha 6 
+   
+   void andar() { //Linha 8
+     System.out.println("Carro andando"); //Linha 9
+   }
+   void parar() { //Linha 11
+     System.out.println("Carro parado"); //Linha 12
+   }
+}
+/*
+Sou um comentário de bloco
+Linha 3 - class: Esta é a palavra-chave que define uma classe em Java. Uma classe é um modelo para criar objetos.
+Linha 3 - Carro: Este é o nome da classe. Neste caso, a classe representa um carro.
+Linhas 4,5,6 - String: Este é o tipo de dados que representa uma sequência de caracteres (texto). cor, marca, modelo: Estas são variáveis de instância ou atributos da classe Carro. Elas representam as características de um carro, como sua cor, marca e modelo.
+Linha 8 - void: Esta palavra-chave indica que o método não retorna nenhum valor.
+Linha 8 - andar: Este é o nome do método. Um método é uma função que pertence a uma classe.
+Linha 9 - System.out.println("Carro andando");: Esta linha imprime a mensagem "Carro andando" no console.
+Linha 11 - void: Esta palavra-chave indica que o método não retorna nenhum valor. parar: Este é o nome do método.
+Linha 12 - System.out.println("Carro parado");: Esta linha imprime a mensagem "Carro parado" no console.
+*/
