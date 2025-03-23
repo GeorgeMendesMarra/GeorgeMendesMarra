7c9e8b141eced41b3cf64fc9efeec1b9a8efd9b7
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-03-23 20:55:17 -0300

    Update ExemploFuncao.java

--- poo_java/ExemploFuncao.java
+++ poo_java/ExemploFuncao.java
@@ -1,16 +1,25 @@
 public class ExemploFuncao {
-   public static void mostrarMensagem1(){
-       System.out.println("Sou uma mensagem muito especial: parte 1");
-       }
-   public static void mostrarMensagem2(){
-       System.out.println("Sou uma mensagem muito especial: parte 2");
-       }  
-       
-   public static void main(String[] args) {
-      System.out.println();
-      mostrarMensagem1();
-      System.out.println();
-      mostrarMensagem2();
-      System.out.println();
-      }
-   }
+
+    // Função que imprime a primeira parte de uma mensagem especial.
+    public static void mostrarMensagem1() {
+        System.out.println("Sou uma mensagem muito especial: parte 1");
+    }
+
+    // Função que imprime a segunda parte de uma mensagem especial.
+    public static void mostrarMensagem2() {
+        System.out.println("Sou uma mensagem muito especial: parte 2");
+    }
+
+    // Função principal que inicia a execução do programa.
+    public static void main(String[] args) {
+        System.out.println(); // Imprime uma linha em branco para melhor formatação.
+
+        mostrarMensagem1(); // Chama a função mostrarMensagem1 para imprimir a primeira parte da mensagem.
+
+        System.out.println(); // Imprime uma linha em branco para melhor formatação.
+
+        mostrarMensagem2(); // Chama a função mostrarMensagem2 para imprimir a segunda parte da mensagem.
+
+        System.out.println(); // Imprime uma linha em branco para melhor formatação.
+    }
+}
