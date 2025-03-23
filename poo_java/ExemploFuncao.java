9a5ff9333468a6cbd60fdcc1e157033fd8cc63ee
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-03-23 20:50:13 -0300

    Add files via upload

---
+++ poo_java/ExemploFuncao.java
@@ -0,0 +1,16 @@
+public class ExemploFuncao {
+   public static void mostrarMensagem1(){
+       System.out.println("Sou uma mensagem muito especial: parte 1");
+       }
+   public static void mostrarMensagem2(){
+       System.out.println("Sou uma mensagem muito especial: parte 2");
+       }  
+       
+   public static void main(String[] args) {
+      System.out.println();
+      mostrarMensagem1();
+      System.out.println();
+      mostrarMensagem2();
+      System.out.println();
+      }
+   }
