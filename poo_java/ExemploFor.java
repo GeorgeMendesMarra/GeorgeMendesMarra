dbaebfb1423b0362396c6857d1e791c1251ce3b5
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-03-23 22:05:41 -0300

    Add files via upload

---
+++ poo_java/ExemploFor.java
@@ -0,0 +1,15 @@
+class ExemploFor {
+    public static void main(String args[]) {
+        // Início de um loop 'for'.
+        // 'int j = 0': Inicializa a variável 'j' com o valor 0.
+        // 'j < 10': Define a condição de continuação do loop. O loop continuará enquanto 'j' for menor que 10.
+        // 'j++': Incrementa o valor de 'j' em 1 após cada iteração do loop.
+        for (int j = 0; j < 10; j++) {
+            // Imprime o valor atual de 'j' no console.
+            System.out.println(j);
+        }
+        // Fim do loop 'for'.
+    }
+    // Fim do método 'main'.
+}
+// Fim da classe 'ExemploFor'.
