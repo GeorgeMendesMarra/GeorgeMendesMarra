072aad09acad574f099ebb6700beca321c9ab73f
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2025-11-01 20:52:14 -0300

    Create hello_world.f90

---
+++ linguagens_de_programacao/fortran/hello_world.f90
@@ -0,0 +1,12 @@
+! Este é um programa básico em Fortran que exibe "Hello World".
+! Em Fortran, o ponto de exclamação (!) marca o início de um comentário de linha única.
+
+program hello_world 
+! A instrução 'program' define o início e o nome do programa principal.
+
+    ! A instrução 'print' é o modo mais simples de saída em Fortran moderno.
+    ! O asterisco (*) em 'print *' indica que o compilador deve usar a formatação padrão.
+    print *, 'Hello World!'
+
+end program hello_world
+! A instrução 'end program' marca o fim do programa, e o nome é opcional, mas recomendado.
