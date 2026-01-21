d3ac1ec3f49f2ce480f2fdcca5afe1103ee42a77
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-01-21 09:06:49 -0300

    Create junta_palavras.js

---
+++ escola_do_futuro/back_end/node_js/junta_palavras.js
@@ -0,0 +1,5 @@
+const parte1 = "Node";
+const parte2 = ".js";
+// Guardamos o resultado da junção em uma terceira variável antes de imprimir
+const resultado = parte1 + parte2;
+console.log(resultado);
