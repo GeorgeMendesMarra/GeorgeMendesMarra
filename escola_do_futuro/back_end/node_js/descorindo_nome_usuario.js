b36caade37a11a684805613dff14bfe61391450a
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-01-21 09:07:47 -0300

    Create descorindo_nome_usuario.js

---
+++ escola_do_futuro/back_end/node_js/descorindo_nome_usuario.js
@@ -0,0 +1,3 @@
+// 'require' serve para importar módulos (ferramentas) que já vêm com o Node
+const os = require('os'); // Importa o módulo de Sistema Operacional (OS)
+console.log("Utilizador atual: " + os.userInfo().username);
