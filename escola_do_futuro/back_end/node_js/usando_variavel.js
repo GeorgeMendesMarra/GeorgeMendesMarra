b19b203a73585b420b5629163b389a4407ad0e79
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-01-21 08:58:46 -0300

    Create usando_variavel.js

---
+++ escola_do_futuro/back_end/node_js/usando_variavel.js
@@ -0,0 +1,4 @@
+// 'const' cria uma variável cujo valor não muda (constante)
+const nome = "George"; 
+// O símbolo '+' aqui serve para juntar (concatenar) o texto com a variável
+console.log("Bem-vindo, " + nome);
