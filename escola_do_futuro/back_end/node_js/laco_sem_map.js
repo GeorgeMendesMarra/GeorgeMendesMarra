1ab9665b03aac8c48eced8bda70ce3e3c9c86bc9
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-02-09 08:27:08 -0300

    Create laco_sem_map.js

---
+++ escola_do_futuro/back_end/node_js/laco_sem_map.js
@@ -0,0 +1,7 @@
+const precosDolar = [10, 20, 30];
+const precosReal = [];
+
+for (let i = 0; i < precosDolar.length; i++) {
+    precosReal.push(precosDolar[i] * 5.5); // Multiplicando manualmente
+}
+console.log(precosReal);
