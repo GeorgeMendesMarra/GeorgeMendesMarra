1a18b77e96f898a1a157e932719d3cac1b267321
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-02-09 08:35:35 -0300

    Create filter1.js

---
+++ escola_do_futuro/back_end/node_js/filter1.js
@@ -0,0 +1,6 @@
+const notas = [4, 8, 5, 10, 7];
+
+// "Filtre as notas onde a nota seja maior ou igual a 7"
+const aprovados = notas.filter(nota => nota >= 7);
+console.log(notas);
+console.log(aprovados);
