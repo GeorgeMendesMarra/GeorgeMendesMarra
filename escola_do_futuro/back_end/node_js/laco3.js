6021e19ed6149fb94d776594cf0023b19af43059
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-02-09 08:14:36 -0300

    Create laco3.js

---
+++ escola_do_futuro/back_end/node_js/laco3.js
@@ -0,0 +1,6 @@
+const linguagens = ['JavaScript', 'Python', 'C++', 'Java'];
+
+// Percorre cada elemento diretamente, sem precisar do índice
+for (const linguagem of linguagens) {
+    console.log(`Linguagem de programação: ${linguagem}`);
+}
