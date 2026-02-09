de7357526cf64bba547517bae082919d74edd670
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-02-09 08:13:21 -0300

    Create laco2.js

---
+++ escola_do_futuro/back_end/node_js/laco2.js
@@ -0,0 +1,6 @@
+let energia = 3;
+// Executa enquanto a condição for verdadeira
+while (energia > 0) {
+    console.log(`Trabalhando... Energia restante: ${energia}`);
+    energia--; // Crucial para o controle da lógica
+}
