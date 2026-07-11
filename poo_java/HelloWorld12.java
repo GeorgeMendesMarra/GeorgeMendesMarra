61aea18d302a1302676bed3e04a3066662d6312c
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 13:26:03 -0300

    Add HelloWorld12 Java program with do-while loop

---
+++ poo_java/HelloWorld12.java
@@ -0,0 +1,14 @@
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld12 { // Classe principal
+    public static void main(String[] args) { // Método principal
+        String mensagem = "Hello World"; // Define a mensagem
+        int contador = 0; // Inicializa o contador
+        do { // Início do loop do-while
+            System.out.println(mensagem); // Imprime a mensagem
+            contador++; // Incrementa o contador
+        } while (contador < 1); // Condição para continuar
+    } // Fim do main
+} // Fim da classe
