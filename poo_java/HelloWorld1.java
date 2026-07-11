d9471eafe857db1fb90ceff8ee6ee7a9f1161e8a
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 12:44:11 -0300

    Refactor HelloWorld1.java with comments and formatting
    Updated HelloWorld1.java to include version and paradigm comments, and improved code formatting.

--- poo_java/HelloWorld1.java
+++ poo_java/HelloWorld1.java
@@ -1,10 +1,9 @@
- public class HelloWorld1 {                   //Linha 1
-     public static void main(String[] args) { //Linha 2
-       System.out.println("Hello, World!");   //Linha 3
-    }
-}
-
-/* //1 - Isso declara uma classe pública chamada HelloWorld. Em Java, todo o código deve estar dentro de uma classe.
-//2 - Este é o método principal, o ponto de entrada do seu programa Java. A assinatura do método é sempre a mesma: public static void main(String[] args).
-//3 - Esta linha de código imprime a string "Hello, World!" no console. System.out é um objeto que representa o fluxo de saída padrão, e println() é um método que imprime uma linha de texto.
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
 */
+public class HelloWorld1 { // Define a classe pública
+    public static void main(String[] args) { // Método principal, ponto de entrada
+        System.out.println("Hello World"); // Imprime a mensagem no console
+    } // Fim do método main
+} // Fim da classe
