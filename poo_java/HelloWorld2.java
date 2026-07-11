0c07893cc41740208eb53dca4e3efe5bb95e88bc
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 12:48:30 -0300

    Refactor HelloWorld2 to use a variable for message
    Updated HelloWorld2.java to include comments and a variable for the message.

--- poo_java/HelloWorld2.java
+++ poo_java/HelloWorld2.java
@@ -1,7 +1,10 @@
-public class HelloWorld2 {
-    public static void main(String[] args) {
-        System.out.print("Hello, World!"); //Linha 3
-    }
-}
-
-/* Linha 3 - apenas imprime a string, sem adicionar uma nova linha. Se você usar System.out.print() várias vezes, a saída aparecerá na mesma linha.*/
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld2 { // Declaração da classe
+    public static void main(String[] args) { // Método principal
+        String mensagem = "Hello World"; // Declara e inicializa uma variável
+        System.out.println(mensagem); // Imprime o conteúdo da variável
+    } // Fim do método
+} // Fim da classe
