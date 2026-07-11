02f3815caa6ff470e75ab4fc7b266004237039cb
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 13:09:10 -0300

    Add HelloWorld9.java to print 'Hello World'
    This Java program defines a class that prints 'Hello World' using a character array.

---
+++ poo_java/HelloWorld9.java
@@ -0,0 +1,11 @@
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld9 { // Classe principal
+    public static void main(String[] args) { // Método principal
+        char[] chars = {'H','e','l','l','o',' ','W','o','r','l','d'}; // Array de caracteres
+        String mensagem = new String(chars); // Converte array para String
+        System.out.println(mensagem); // Imprime a string
+    } // Fim do main
+} // Fim da classe
