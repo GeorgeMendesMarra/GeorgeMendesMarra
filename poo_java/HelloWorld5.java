8ce150a7d5cf37320073529a5cd7b180c17fd800
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 12:56:34 -0300

    Update HelloWorld5 to initialize message statically
    Refactor HelloWorld5 to use a static block for initialization.

--- poo_java/HelloWorld5.java
+++ poo_java/HelloWorld5.java
@@ -1,9 +1,15 @@
-public class HelloWorld5 {
-    public static void main(String[] args) {
-        System.out.printf("%s\n", "Hello, World!"); //Linha 3
-    }
-}
-
-/* Linha 3 - System.out.printf() permite formatação de string semelhante à função printf() em C.
-   %s é um especificador de formato para strings, e \n representa uma nova linha.
-*/
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld5 { // Classe principal
+    private static String mensagem; // Variável estática
+    
+    static { // Bloco estático (executado quando a classe é carregada)
+        mensagem = "Hello World"; // Inicializa a variável
+    } // Fim do bloco estático
+    
+    public static void main(String[] args) { // Método principal
+        System.out.println(mensagem); // Imprime a mensagem
+    } // Fim do main
+} // Fim da classe
