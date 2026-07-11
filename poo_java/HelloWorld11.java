c2e1cf5cc9bb99bb9c2ea50ae5de14f54132173e
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 13:13:25 -0300

    Add HelloWorld11 class with main method

---
+++ poo_java/HelloWorld11.java
@@ -0,0 +1,14 @@
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld11 { // Classe principal
+    public static void main(String[] args) { // Método principal
+        String mensagem = "Hello World"; // Define a mensagem
+        int contador = 0; // Inicializa o contador
+        while (contador < 1) { // Enquanto contador for menor que 1
+            System.out.println(mensagem); // Imprime a mensagem
+            contador++; // Incrementa o contador
+        } // Fim do while
+    } // Fim do main
+} // Fim da classe
