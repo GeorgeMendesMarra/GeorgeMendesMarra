5640dafaa0d6aae7d99f930dad4942f50d02cad3
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 12:59:11 -0300

    Add HelloWorld6 class with main method

---
+++ poo_java/HelloWorld6.java
@@ -0,0 +1,11 @@
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld6 { // Classe principal
+    public static void main(String[] args) { // Método principal
+        StringBuilder sb = new StringBuilder(); // Cria um StringBuilder
+        sb.append("Hello World"); // Adiciona a string ao builder
+        System.out.println(sb.toString()); // Converte para String e imprime
+    } // Fim do main
+} // Fim da classe
