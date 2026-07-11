96c788cd996956eef45df40ff092bd234c115369
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 13:11:25 -0300

    Add HelloWorld10 class with main method

---
+++ poo_java/HelloWorld10.java
@@ -0,0 +1,12 @@
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld10 { // Classe principal
+    public static void main(String[] args) { // Método principal
+        String mensagem = "Hello World"; // Define a mensagem
+        for (int i = 0; i < 1; i++) { // Loop que executa uma única vez
+            System.out.println(mensagem); // Imprime a mensagem
+        } // Fim do for
+    } // Fim do main
+} // Fim da classe
