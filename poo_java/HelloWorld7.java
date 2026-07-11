d4bd51695de676a93d6c4b17bc4126196071ed4f
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 13:01:17 -0300

    Add HelloWorld7 class with main method

---
+++ poo_java/HelloWorld7.java
@@ -0,0 +1,10 @@
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld7 { // Classe principal
+    public static void main(String[] args) { // Método principal
+        String mensagem = String.format("%s %s", "Hello", "World"); // Formata a string
+        System.out.println(mensagem); // Imprime o resultado
+    } // Fim do main
+} // Fim da classe
