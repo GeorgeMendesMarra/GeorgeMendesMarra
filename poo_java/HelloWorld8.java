7095b3ebab248b98eca77b85df1153e8a7165a56
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 13:03:51 -0300

    Add HelloWorld8 Java program

---
+++ poo_java/HelloWorld8.java
@@ -0,0 +1,11 @@
+/*
+ * Versão Java: 8+
+ * Paradigma: Imperativo
+ */
+public class HelloWorld8 { // Classe principal
+    public static void main(String[] args) { // Método principal
+        String parte1 = "Hello"; // Primeira parte
+        String parte2 = "World"; // Segunda parte
+        System.out.println(parte1 + " " + parte2); // Concatena e imprime
+    } // Fim do main
+} // Fim da classe
