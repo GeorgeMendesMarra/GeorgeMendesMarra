8674a355f3f698d897b12636bd584e54d4fc667b
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-07-11 15:43:11 -0300

    Add HelloWorld26.java with basic Hello World program

---
+++ poo_java/HelloWorld26.java
@@ -0,0 +1,10 @@
+/*
+ * Versão Java: 10+
+ * Paradigma: Imperativo (inferência de tipos)
+ */
+public class HelloWorld26 { // Classe principal
+    public static void main(String[] args) { // Método principal
+        var mensagem = "Hello World"; // Usa var para inferência de tipo
+        System.out.println(mensagem); // Imprime a mensagem
+    } // Fim do main
+} // Fim da classe
